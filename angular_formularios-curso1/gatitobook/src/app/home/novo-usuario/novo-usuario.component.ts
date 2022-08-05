3426a6adc2031ac70009c2b78de92ad5c14897d3
Daniel Silva <daniel.silva@qualityautomacao.com>
2022-08-05 13:44:31 -0300

    Add Validators

---
+++ angular_formularios-curso1/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculo.validator';
+import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
+
+@Component({
+	selector: 'app-novo-usuario',
+	templateUrl: './novo-usuario.component.html',
+	styleUrls: ['./novo-usuario.component.css']
+})
+export class NovoUsuarioComponent implements OnInit {
+
+	novoUsuarioForm!: FormGroup;
+	constructor(private formBuilder: FormBuilder, private novoUsuarioService: NovoUsuarioService) { }
+
+	ngOnInit(): void {
+		this.novoUsuarioForm = this.formBuilder.group({
+			userName: ['', [
+				minusculoValidator
+			]],
+			email: ['', [
+				Validators.required, Validators.email
+			]],
+			fullName: ['', [
+				Validators.required, Validators.minLength(4)
+			]],
+			password: ['', [
+				Validators.required
+			]],
+		})
+	}
+	cadastrar() {
+		const novoUsuario = this.novoUsuarioForm?.getRawValue() as NovoUsuario;
+	}
+}
