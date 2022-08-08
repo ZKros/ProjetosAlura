a90ffa8af14d357de9417d91f60648187fd21905
Daniel Silva <daniel.silva@qualityautomacao.com>
2022-08-08 14:27:02 -0300

    Projeto2 - Angular

---
+++ angular_formularios-curso2/gatitobook/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
+import { RodapeModule } from './componentes/rodape/rodape.module';
+import { HomeModule } from './home/home.module';
+
+@NgModule({
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HomeModule,
+		HttpClientModule,
+		CabecalhoModule,
+		RodapeModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
