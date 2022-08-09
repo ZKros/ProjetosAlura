5d725006a386d0561aea22646c42700dda3c562d
Daniel Silva <daniel.silva@qualityautomacao.com>
2022-08-09 14:18:06 -0300

    Add Guard

--- angular_formularios-curso2/gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
+++ angular_formularios-curso2/gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, switchMap } from 'rxjs';
+import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
+import { Animais } from '../animais';
+import { AnimaisService } from '../animais.service';
 
 @Component({
-  selector: 'app-lista-animais',
-  templateUrl: './lista-animais.component.html',
-  styleUrls: ['./lista-animais.component.css']
+	selector: 'app-lista-animais',
+	templateUrl: './lista-animais.component.html',
+	styleUrls: ['./lista-animais.component.css']
 })
 export class ListaAnimaisComponent implements OnInit {
 
-  constructor() { }
+	animais$ !: Observable<Animais>;
 
-  ngOnInit(): void {
-  }
+	constructor(private usuarioService: UsuarioService, private animaisService: AnimaisService) { }
 
+	ngOnInit(): void {
+		this.animais$ = this.usuarioService.retornaUsuario().pipe(
+			switchMap((usuario) => {
+				const userName = usuario.name ?? '';
+				return this.animaisService.listaDoUsuario(userName);
+			})
+		)
+	}
 }
